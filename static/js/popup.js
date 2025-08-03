677ebe4b8c8e3309d74cf80d619525d2637faa02
shipali k <preethikotyan17gmail.com>
2025-08-03 18:42:31 +0530

    Final updated portfolio: reduced paragraph size and styling changes

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,4 +1,3 @@
-// Show image in popup when "View" is clicked
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll('.btn-view').forEach(btn => {
     btn.addEventListener('click', function (e) {
@@ -10,12 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Close the popup
+
 function closePopup() {
   document.getElementById('popup').style.display = 'none';
   document.getElementById('popup-img').src = '';
 }
-
-document.getElementById('menuToggle').addEventListener('click', function () {
-  document.querySelector('.sidebar').classList.toggle('active');
-});
+  const menuToggle = document.getElementById("menuToggle");
+    const sidebar = document.querySelector(".sidebar");
+    menuToggle.addEventListener("click", () => {
+      sidebar.classList.toggle("active");
+    });
